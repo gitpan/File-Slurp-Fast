7ab6f05fff8507b6e9e36149ba0c928adc615b91
Alexis Sukrieh <sukria@sukria.net>
2014-03-20 23:17:38 +0000

    Import of SUKRIA/File-Slurp-Fast-0.002 from CPAN.
    gitpan-cpan-distribution: File-Slurp-Fast
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         SUKRIA/File-Slurp-Fast-0.002.tar.gz
    gitpan-cpan-author:       SUKRIA
    gitpan-cpan-maturity:     released

--- lib/File/Slurp/Fast.pm
+++ lib/File/Slurp/Fast.pm
@@ -115,11 +115,11 @@ File::Slurp::Fast - file parser intended for big files that doesn't fit into mai
 
 =head1 VERSION
 
-version 0.001
+version 0.002
 
 =head1 DESCRIPTION
 
-In most of the cases, you don't want to use this, but L<File::Slurp> instead.
+In most of the cases, you don't want to use this, but L<File::Slurpi::Tiny> instead.
 
 This class is able to slurp a line from a file without loading the whole file in
 memory. When you want to deal with files of millions of lines, on a limited
